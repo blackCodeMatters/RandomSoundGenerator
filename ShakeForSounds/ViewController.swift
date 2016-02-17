a8e8e530447e087beee5d967e6a1eb0a9b31b0e8
Dustin M <Eleuthera@Dustins-Air.T-mobile.com>
2016-02-16 22:51:43 -0500

    first commit

--- ShakeForSounds/ViewController.swift
+++ ShakeForSounds/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    var array = ["rm_wubba", "rm_wrecked", "rm_tinyrick", "rm_showme", "rm_rubber", "rm_myman", "rm_ilike"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event!.subtype == UIEventSubtype.MotionShake {
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(array.count)))
+            
+            var audioPath = NSBundle.mainBundle().pathForResource(array[randomNumber], ofType: "wav")
+            
+            var error: NSError? = nil
+
+            do {
+                
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath!))
+            
+            player.play()
+            
+            }catch{
+            //process errors here
+            }
+            
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
